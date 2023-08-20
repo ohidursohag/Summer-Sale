5d097c2f20461fe2c67d306ffc738e805026de0e
ohidursohag <ohidursohag9595@gmail.com>
2023-08-20 19:59:39 +0600

    Full responsive and Iteractive update

--- Js/summerSale.js
+++ Js/summerSale.js
@@ -1,1 +1,55 @@
-console.log('Summer');
+// Add cart items on list and Total price
+let getTotal = 0;
+function cardItemClick(target) {
+   const getProductName = target.children[2].innerText;
+   const getProductPrice = target.children[3].children[0].innerText
+   AddCartListItems(getProductName);
+   getTotal += Number(getProductPrice);
+   displayValue('totalPrice', getTotal);
+   displayValue('finalTotal', getTotal);
+   if (getTotal >= 200) {
+      btnCouponApply.disabled = false; 
+      couponCode.disabled = false; 
+   }
+   if (getTotal > 0) {
+      bntPurchase.disabled = false; 
+   }
+}
+
+//Get and Apply discount
+btnCouponApply.addEventListener('click', function () {
+   if (couponCode.value ==='SELL200') {
+      const discount = getTotal * 0.2;
+      displayValue('discountPrice', discount);
+      const finalTotalPrice = getTotal - discount;
+      displayValue('finalTotal', finalTotalPrice);
+      couponCode.value = '';
+   } else {
+      alert('Please input a valid Coupon');
+      couponCode.value = '';
+      return;
+   }
+});
+
+// Clear All Cart and Price Data click on Go Home Buton
+function clearAll() {
+   clearValue('totalPrice');
+   clearValue('discountPrice');
+   clearValue('finalTotal');
+   cartItemListContainer.innerHTML = '';
+   bntPurchase.disabled = true;
+   btnCouponApply.disabled = true;
+   couponCode.disabled = true;
+   getTotal = 0;
+}
+
+// input Coupon Code by clicking on Promo code
+function inputCouponCode() {
+   if (getTotal >= 200) {
+      couponCode.value = 'SELL200'
+   } else {
+      alert(`You Have to Purchase TK 200 or above & get 20 % off`);
+   }
+}
+
+
--- Js/utilities.js
+++ Js/utilities.js
@@ -1,1 +1,33 @@
-console.log('utilitis');
+const cartItemListContainer = document.getElementById('cartItemListContainer');
+
+const bntPurchase = document.getElementById('bntPurchase');
+const btnCouponApply = document.getElementById('btnCouponApply');
+const couponCode = document.getElementById('couponCode');
+
+// initially Disabled the cuponApply and Make Purchase buttons
+bntPurchase.disabled = true;
+btnCouponApply.disabled = true;
+couponCode.disabled = true;
+
+// Add items on Cart list
+function AddCartListItems(itemName) {
+   const count = cartItemListContainer.childElementCount;
+   let item = document.createElement('p');
+   item.innerHTML = `${count + 1}. ${itemName}`;
+   cartItemListContainer.appendChild(item);
+}
+
+// Show Values
+function displayValue(id, value) {
+   const getElement = document.getElementById(id);
+   getElement.innerText = value.toFixed(2);
+}
+
+// clear Values
+function clearValue(id) {
+   const getElement = document.getElementById(id);
+   getElement.innerText = '0.00';
+}
+
+
+
